16b0bb1fa9f62843451fd7f0ec9a4ff10f0080cb
vinaykharayat <vinaykharayat@gmail.com>
2021-01-29 10:33:44 +0530

    fixed  menu animation and visibility

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
 let isHidden = false;
 document.getElementById('nav').addEventListener("click", function() {
-        if (isHidden) { //when hamburger is clicked and cross is appearing
-            // $(this).next().slideToggle();
-            document.getElementById("droppingList").style.top = 0;
-            // $(this).next().animate({
-            //     top: "0%"
-            // }, "fast", "swing")
+        if (isHidden) {
 
-            // $("#para").fadeIn();
             document.getElementById("leftColor").style.transition = "1s";
             document.getElementById("leftColor").style.width = "0%";
             document.getElementById("rightColor").style.transition = "1s";
@@ -15,27 +9,26 @@ document.getElementById('nav').addEventListener("click", function() {
             var opacity = 0;
             var intervalID = setInterval(function() {
                 if (opacity < 1) {
-                    opacity = opacity + 0.2
+                    opacity += 0.2
                     document.getElementById('para').style.opacity = opacity;
                 } else {
                     clearInterval(intervalID);
                 }
             }, 50);
+            var droppingListOp = 1;
+            var intervalID2 = setInterval(function() {
+                if (droppingListOp >= 0) {
+                    droppingListOp -= 0.2;
+                    document.getElementById('droppingList').style.opacity = droppingListOp;
+                } else {
+                    clearInterval(intervalID2);
+                }
+            }, 10);
 
             document.getElementById("branding").style.textShadow = "unset"
-
-
-            // $("#leftColor").animate({
-            //     width: "0%",
-            //     backgroundColor: "blue"
-            // }, 'fast', "swing")
-            // $("#branding").css({ "text-shadow": "unset" })
-            // $("#rightColor").animate({
-            //     width: "0%",
-            //     backgroundColor: "blue"
-            // }, 'fast', "swing")
+            document.getElementById("droppingList").style.transition = "1s"
             document.getElementById("droppingList").style.top = "0%"
-            document.getElementById("droppingList").style.transition = "2s"
+                // document.getElementById("droppingList").style.display = "none"
 
             document.getElementById("second").style.left = "0px";
             document.getElementById("second").style.transition = "500";
@@ -46,7 +39,26 @@ document.getElementById('nav').addEventListener("click", function() {
             document.getElementById("third").style.transform = "rotate(0deg)";
             document.getElementById("third").style.transition = "0.3s";
             document.getElementById("third").style.transformOrigin = "0 0";
-            document.getElementById("droppingList").style.opacity = "0"
+            isHidden = false;
+
+            //when hamburger is clicked and cross is appearing
+            // $(this).next().slideToggle();
+            // document.getElementById("droppingList").style.top = 0;
+            // $(this).next().animate({
+            //     top: "0%"
+            // }, "fast", "swing")
+
+            // $("#para").fadeIn();
+
+            // $("#leftColor").animate({
+            //     width: "0%",
+            //     backgroundColor: "blue"
+            // }, 'fast', "swing")
+            // $("#branding").css({ "text-shadow": "unset" })
+            // $("#rightColor").animate({
+            //     width: "0%",
+            //     backgroundColor: "blue"
+            // }, 'fast', "swing")
 
             // $("#second").animate({
             //     left: "0px",
@@ -58,36 +70,39 @@ document.getElementById('nav').addEventListener("click", function() {
             // $("#third").css({ 'transform-origin': '0 0' })
             // $("#droppingList").hide();
 
-            isHidden = false;
+
         } else {
 
-            // $("#leftColor").animate({
-            //     width: "30%",
-            //     backgroundColor: "blue"
-            // }, 'fast', "swing")
             document.getElementById("leftColor").style.transition = "1s";
             document.getElementById("leftColor").style.width = "30%";
             document.getElementById("rightColor").style.transition = "1s";
             document.getElementById("rightColor").style.width = "30%";
             document.getElementById("branding").style.textShadow = "2px 2px black"
-                // $("#branding").css({ "text-shadow": "2px 2px black" })
-                // $("#rightColor").animate({
-                //     width: "30%",
-                //     backgroundColor: "blue"
-                // }, 'fast', "swing")
+
 
             var opacity = 1;
             var intervalID = setInterval(function() {
                 if (opacity >= 0) {
-                    opacity = opacity - 0.2
+                    opacity -= 0.2
                     document.getElementById('para').style.opacity = opacity;
                 } else {
                     clearInterval(intervalID);
                 }
             }, 50);
-
+            var droppingListOp = 0;
+            var intervalID2 = setInterval(function() {
+                if (droppingListOp < 1) {
+                    droppingListOp += 0.2
+                    document.getElementById('droppingList').style.opacity = droppingListOp;
+                } else {
+                    clearInterval(intervalID2);
+                }
+            }, 10);
+            document.getElementById("droppingList").style.transition = "1s"
             document.getElementById("droppingList").style.top = "50%"
-            document.getElementById("droppingList").style.transition = "2s"
+                // document.getElementById("droppingList").style.border = "solid"
+                // document.getElementById("droppingList").style.display = "block"
+
 
             document.getElementById("second").style.transition = "1s";
             document.getElementById("second").style.left = "-100px";
@@ -99,7 +114,25 @@ document.getElementById('nav').addEventListener("click", function() {
             document.getElementById("third").style.transform = "rotate(-50deg)";
             document.getElementById("third").style.transition = "0.3s";
             document.getElementById("third").style.transformOrigin = "5% 40%";
-            document.getElementById("droppingList").style.opacity = "1"
+
+            isHidden = true;
+
+            // $("#leftColor").animate({
+            //     width: "30%",
+            //     backgroundColor: "blue"
+            // }, 'fast', "swing")
+
+
+
+            // $("#branding").css({ "text-shadow": "2px 2px black" })
+            // $("#rightColor").animate({
+            //     width: "30%",
+            //     backgroundColor: "blue"
+            // }, 'fast', "swing")
+
+
+
+            // document.getElementById("droppingList").style.opacity = "1"
 
             // $(this).next().animate({
             //     top: "50%"
@@ -115,7 +148,7 @@ document.getElementById('nav').addEventListener("click", function() {
             // $("#third").css({ 'transform-origin': '5% 40%' })
             // $("#droppingList").show();
 
-            isHidden = true;
+
         }
     })
     // $("#nav").on("click", function() {
