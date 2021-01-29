071db62387cd1a9d388d47f71db0f9bd547d30e2
vinaykharayat <vinaykharayat@gmail.com>
2021-01-29 09:51:00 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,61 +1,125 @@
 let isHidden = false;
-$("#nav").on("click", function() {
+document.getElementById('nav').addEventListener("click", function() {
+        if (isHidden) { //when hamburger is clicked and cross is appearing
+            // $(this).next().slideToggle();
+            document.getElementById("droppingList").style.top = 0;
+            // $(this).next().animate({
+            //     top: "0%"
+            // }, "fast", "swing")
 
-    if (isHidden) { //when hamburger is clicked and cross is appearing
-        // $(this).next().slideToggle();
-        $(this).next().animate({
-            top: "0%"
-        }, "fast", "swing")
+            // $("#para").fadeIn();
+            document.getElementById("leftColor").style.transition = "1s";
+            document.getElementById("leftColor").style.width = "0%";
+            document.getElementById("rightColor").style.transition = "1s";
+            document.getElementById("rightColor").style.width = "0%";
+            var opacity = 0;
+            var intervalID = setInterval(function() {
+                if (opacity < 1) {
+                    opacity = opacity + 0.2
+                    document.getElementById('para').style.opacity = opacity;
+                } else {
+                    clearInterval(intervalID);
+                }
+            }, 50);
 
-        $("#para").fadeIn();
-        $("#leftColor").animate({
-            width: "0%",
-            backgroundColor: "blue"
-        }, 'fast', "swing")
-        $("#branding").css({ "text-shadow": "unset" })
-        $("#rightColor").animate({
-            width: "0%",
-            backgroundColor: "blue"
-        }, 'fast', "swing")
+            document.getElementById("branding").style.textShadow = "unset"
 
-        $("#second").animate({
-            left: "0px",
-            opacity: '100%'
-        }, 'fast', "swing")
-        $("#first").css({ 'transform': 'rotate(' + 0 + 'deg)', "transition": "0.3s" })
-        $("#first").css({ 'transform-origin': '0 0' })
-        $("#third").css({ 'transform': 'rotate(' + 0 + 'deg)', "transition": "0.3s" })
-        $("#third").css({ 'transform-origin': '0 0' })
-        $("#droppingList").hide();
 
-        isHidden = false;
-    } else {
+            // $("#leftColor").animate({
+            //     width: "0%",
+            //     backgroundColor: "blue"
+            // }, 'fast', "swing")
+            // $("#branding").css({ "text-shadow": "unset" })
+            // $("#rightColor").animate({
+            //     width: "0%",
+            //     backgroundColor: "blue"
+            // }, 'fast', "swing")
+            document.getElementById("droppingList").style.top = "0%"
+            document.getElementById("droppingList").style.transition = "2s"
 
-        $("#leftColor").animate({
-            width: "30%",
-            backgroundColor: "blue"
-        }, 'fast', "swing")
-        $("#branding").css({ "text-shadow": "2px 2px black" })
-        $("#rightColor").animate({
-            width: "30%",
-            backgroundColor: "blue"
-        }, 'fast', "swing")
-        $("#para").fadeOut();
-        $(this).next().animate({
-            top: "50%"
-        }, "fast", "swing")
+            document.getElementById("second").style.left = "0px";
+            document.getElementById("second").style.transition = "500";
+            document.getElementById("second").style.opacity = "100%";
+            document.getElementById("first").style.transform = "rotate(0deg)";
+            document.getElementById("first").style.transition = "0.3s";
+            document.getElementById("first").style.transformOrigin = "0 0";
+            document.getElementById("third").style.transform = "rotate(0deg)";
+            document.getElementById("third").style.transition = "0.3s";
+            document.getElementById("third").style.transformOrigin = "0 0";
+            document.getElementById("droppingList").style.opacity = "0"
 
-        $("#second").animate({
-            left: "-100px",
-            opacity: '0'
-        }, 'fast', "swing")
-        $("#first").css({ 'transform': 'rotate(' + 50 + 'deg)', "transition": "0.3s" })
-        $("#first").css({ 'transform-origin': '5% 40%' })
-        $("#third").css({ 'transform': 'rotate(' + -50 + 'deg)', "transition": "0.3s" })
-        $("#third").css({ 'transform-origin': '5% 40%' })
-        $("#droppingList").show();
+            // $("#second").animate({
+            //     left: "0px",
+            //     opacity: '100%'
+            // }, 'fast', "swing")
+            // $("#first").css({ 'transform': 'rotate(' + 0 + 'deg)', "transition": "0.3s" })
+            // $("#first").css({ 'transform-origin': '0 0' })
+            // $("#third").css({ 'transform': 'rotate(' + 0 + 'deg)', "transition": "0.3s" })
+            // $("#third").css({ 'transform-origin': '0 0' })
+            // $("#droppingList").hide();
 
-        isHidden = true;
-    }
+            isHidden = false;
+        } else {
 
-})
+            // $("#leftColor").animate({
+            //     width: "30%",
+            //     backgroundColor: "blue"
+            // }, 'fast', "swing")
+            document.getElementById("leftColor").style.transition = "1s";
+            document.getElementById("leftColor").style.width = "30%";
+            document.getElementById("rightColor").style.transition = "1s";
+            document.getElementById("rightColor").style.width = "30%";
+            document.getElementById("branding").style.textShadow = "2px 2px black"
+                // $("#branding").css({ "text-shadow": "2px 2px black" })
+                // $("#rightColor").animate({
+                //     width: "30%",
+                //     backgroundColor: "blue"
+                // }, 'fast', "swing")
+
+            var opacity = 1;
+            var intervalID = setInterval(function() {
+                if (opacity >= 0) {
+                    opacity = opacity - 0.2
+                    document.getElementById('para').style.opacity = opacity;
+                } else {
+                    clearInterval(intervalID);
+                }
+            }, 50);
+
+            document.getElementById("droppingList").style.top = "50%"
+            document.getElementById("droppingList").style.transition = "2s"
+
+            document.getElementById("second").style.transition = "1s";
+            document.getElementById("second").style.left = "-100px";
+
+            document.getElementById("second").style.opacity = "0";
+            document.getElementById("first").style.transform = "rotate(50deg)";
+            document.getElementById("first").style.transition = "0.3s";
+            document.getElementById("first").style.transformOrigin = "5% 40%";
+            document.getElementById("third").style.transform = "rotate(-50deg)";
+            document.getElementById("third").style.transition = "0.3s";
+            document.getElementById("third").style.transformOrigin = "5% 40%";
+            document.getElementById("droppingList").style.opacity = "1"
+
+            // $(this).next().animate({
+            //     top: "50%"
+            // }, "fast", "swing")
+
+            // $("#second").animate({
+            //     left: "-100px",
+            //     opacity: '0'
+            // }, 'fast', "swing")
+            // $("#first").css({ 'transform': 'rotate(' + 50 + 'deg)', "transition": "0.3s" })
+            // $("#first").css({ 'transform-origin': '5% 40%' })
+            // $("#third").css({ 'transform': 'rotate(' + -50 + 'deg)', "transition": "0.3s" })
+            // $("#third").css({ 'transform-origin': '5% 40%' })
+            // $("#droppingList").show();
+
+            isHidden = true;
+        }
+    })
+    // $("#nav").on("click", function() {
+
+
+
+// })
